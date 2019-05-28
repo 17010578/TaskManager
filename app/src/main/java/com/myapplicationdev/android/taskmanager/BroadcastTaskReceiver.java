6ccb1545b0bc411d9cf4cb0f85ac22c4809e2251
MuhdFakhry <15007803@myrp.edu.sg>
2019-05-28 15:23:01 +0800

    Add BroadcastTaskReceiver ver 2.0

--- app/src/main/java/com/myapplicationdev/android/taskmanager/BroadcastTaskReceiver.java
+++ app/src/main/java/com/myapplicationdev/android/taskmanager/BroadcastTaskReceiver.java
@@ -7,7 +7,11 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
+import android.os.VibrationEffect;
 import android.support.v4.app.NotificationCompat;
 
 public class BroadcastTaskReceiver extends BroadcastReceiver {
@@ -20,6 +24,9 @@ public class BroadcastTaskReceiver extends BroadcastReceiver {
 
         String name = intent.getStringExtra("name");
 
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        long[] v = {500,1000};
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel("default", "Default Channel", NotificationManager.IMPORTANCE_DEFAULT);
 
@@ -30,13 +37,23 @@ public class BroadcastTaskReceiver extends BroadcastReceiver {
         Intent i = new Intent(context,MainActivity.class);
         PendingIntent pIntent = PendingIntent.getActivity(context, reqCode, i, PendingIntent.FLAG_CANCEL_CURRENT);
 
+        // BigText style notification
+        NotificationCompat.BigTextStyle bigText = new NotificationCompat.BigTextStyle();
+        bigText.setBigContentTitle("Task Manager Reminder");
+        bigText.bigText("");
+        bigText.setSummaryText("New Task Added");
+
         // Build notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
         builder.setContentTitle("Task Manager Reminder");
         builder.setContentText(name);
         builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+        builder.setStyle(bigText);
         builder.setContentIntent(pIntent);
         builder.setAutoCancel(true);
+        builder.setSound(soundUri);
+        builder.setVibrate(v);
+        builder.setLights(Color.RED, 3000, 3000);
 
         Notification n = builder.build();
 
